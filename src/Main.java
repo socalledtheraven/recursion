5096655098c2e317939631659f73d421e5257769
Tom <tom.dalrymple3@gmail.com>
2023-02-08 10:15:34 +0000

    ads

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] data = {13, 13, 18, 19, 23, 38, 57, 70, 79, 88, 89, 94, 102, 108, 122, 125, 127, 177, 180, 201, 210, 221, 226, 226, 234, 237, 247, 293, 295, 297, 303, 317, 320, 339, 343, 373, 375, 378, 378, 380, 403, 405, 407, 408, 412, 440, 466, 479, 485, 486, 493, 504, 509, 525, 537, 564, 565, 570, 578, 581, 584, 589, 605, 611, 617, 621, 650, 665, 669, 692, 696, 714, 742, 756, 756, 763, 766, 776, 780, 789, 817, 857, 881, 882, 883, 885, 897, 898, 906, 909, 911, 916, 929, 929, 939, 941, 958, 985, 988, 994};
-        System.out.println(recursiveBinarySearch(data, 102));
-    }
-
-    public static int[] getSlice(int[] array, int startIndex, int endIndex) {
-        // Get the slice of the Array
-        int[] slicedArray = new int[endIndex - startIndex];
-        // copying array elements from the original array to the newly created sliced array
-        System.arraycopy(array, startIndex, slicedArray, 0, slicedArray.length);
-        // returns the slice of an array
-        return slicedArray;
+        System.out.println(recursiveBinarySearch(data, 916, 0, data.length-1));
     }
 
     public static int fib(int n) {
@@ -41,8 +32,9 @@ public class Main {
         return -1;
     }
 
-    public static int recursiveBinarySearch(int[] arr, int n) {
-        int midpoint = (int)(arr.length/2);
+    public static int recursiveBinarySearch(int[] arr, int n, int upper, int lower) {
+        int midpoint = (int)(upper+lower/2);
+        System.out.println("mid: " + midpoint);
         if (arr[midpoint] == n) {
             return midpoint;
         } else if (arr.length == 1) {
@@ -50,9 +42,11 @@ public class Main {
         }
 
         if (arr[midpoint] > n) {
-            recursiveBinarySearch(getSlice(arr, 0, midpoint), n);
+            System.out.println("lower, " + arr[midpoint]);
+            return recursiveBinarySearch(arr, n, 0, midpoint);
         } else if (arr[midpoint] < n) {
-            recursiveBinarySearch(getSlice(arr, midpoint, arr.length), n);
+            System.out.println("higher, " + arr[midpoint]);
+            return recursiveBinarySearch(arr, n, midpoint, arr.length-1);
         }
         return -1;
     }
