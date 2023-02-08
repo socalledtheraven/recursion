703a645b3a86950b92efd2f3106996469f9a7ed0
socalledtheraven <tom.dalrymple3@gmail.com>
2023-02-08 18:54:10 +0000

    recursive works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] data = {13, 13, 18, 19, 23, 38, 57, 70, 79, 88, 89, 94, 102, 108, 122, 125, 127, 177, 180, 201, 210, 221, 226, 226, 234, 237, 247, 293, 295, 297, 303, 317, 320, 339, 343, 373, 375, 378, 378, 380, 403, 405, 407, 408, 412, 440, 466, 479, 485, 486, 493, 504, 509, 525, 537, 564, 565, 570, 578, 581, 584, 589, 605, 611, 617, 621, 650, 665, 669, 692, 696, 714, 742, 756, 756, 763, 766, 776, 780, 789, 817, 857, 881, 882, 883, 885, 897, 898, 906, 909, 911, 916, 929, 929, 939, 941, 958, 985, 988, 994};
-        System.out.println(recursiveBinarySearch(data, 916, 0, data.length-1));
+        System.out.println(recursiveBinarySearch(data, 916, data.length-1, 0));
     }
 
     public static int fib(int n) {
@@ -33,7 +33,7 @@ public class Main {
     }
 
     public static int recursiveBinarySearch(int[] arr, int n, int upper, int lower) {
-        int midpoint = (int)(upper+lower/2);
+        int midpoint = (int)((upper+lower)/2);
         System.out.println("mid: " + midpoint);
         if (arr[midpoint] == n) {
             return midpoint;
@@ -42,11 +42,13 @@ public class Main {
         }
 
         if (arr[midpoint] > n) {
+            upper = midpoint-1;
             System.out.println("lower, " + arr[midpoint]);
-            return recursiveBinarySearch(arr, n, 0, midpoint);
+            return recursiveBinarySearch(arr, n, upper, lower);
         } else if (arr[midpoint] < n) {
+            lower = midpoint+1;
             System.out.println("higher, " + arr[midpoint]);
-            return recursiveBinarySearch(arr, n, midpoint, arr.length-1);
+            return recursiveBinarySearch(arr, n, upper, lower);
         }
         return -1;
     }
